8998a0b15ff77ac781165f2a55cedfc0f7da15c2
agustin <agustincresta@gmail.com>
2021-06-25 15:33:11 -0300

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 3900
+var port =  server.listen(process.env.PORT || 3900);
 
 mongoose.set('useFindAndModify', false)
 mongoose.Promise = global.Promise;
